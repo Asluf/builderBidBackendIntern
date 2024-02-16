140e54bd33cb9eb9affcf30771532bb1e6395509
Asluf <ahmedasluf@gmail.com>
2024-02-16 12:04:06 +0530

    added the get_floor_plan api

--- server/controllers/UserController.ts
+++ server/controllers/UserController.ts
@@ -1,12 +1,11 @@
-import express, { Request, Response } from 'express';
-import Uploads from '../models/UploadsModel';
-import Floorplan from '../models/FloorplanModel';
+import express, { Request, Response } from "express";
+import Uploads from "../models/UploadsModel";
+import Floorplan from "../models/FloorplanModel";
+import mongoose from "mongoose";
 
-
-export const getUser = function(req: Request, res: Response) {
+export const getUser = function (req: Request, res: Response) {
   res.status(200).json({ message: "User API is working!" });
 };
-
 
 export const saveFloorPlan = async (req: any, res: any) => {
   try {
@@ -16,23 +15,68 @@ export const saveFloorPlan = async (req: any, res: any) => {
     }
 
     const { floorplan_name, type, scale, notes } = req.body;
-    const upload = new Uploads({ img_path: file.path});
+    const upload = new Uploads({ img_path: file.path });
     const savedUpload = await upload.save();
 
     const floorplan = new Floorplan({
       floorplan_name,
       type,
       scale,
-      image_id: savedUpload._id, 
+      image_id: savedUpload._id,
       notes,
     });
 
     const savedFloorplan = await floorplan.save();
 
-    res.status(201).json({ message: 'Floorplan saved successfully', savedFloorplan });
+    res
+      .status(201)
+      .json({ message: "Floorplan saved successfully", savedFloorplan });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
+// get one floor plan according to id
+export const getFloorPlan = function (req: Request, res: Response) {
+  const planId = req.params.plan_id;
 
+  if (!planId || planId.length !== 24) {
+    return res.status(400).json({ success: false, message: "Invalid plan_id" });
+  }
+
+  Floorplan.aggregate([
+    {
+      $lookup: {
+        from: "uploads",
+        localField: "image_id",
+        foreignField: "_id",
+        as: "floorPlanImage",
+      },
+    },
+    { $unwind: "$floorPlanImage" },
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId.createFromHexString(planId),
+      },
+    },
+  ])
+    .then((data) => {
+      if (!data || data.length === 0) {
+        return res
+          .status(404)
+          .json({ success: false, message: "Floor plan not found!" });
+      }
+      return res.status(200).json({
+        success: true,
+        message: `Floor plan found`,
+        data: data,
+      });
+    })
+    .catch((err) => {
+      return res.status(500).json({
+        success: true,
+        message: "Something went wrong",
+        data: err,
+      });
+    });
+};
--- server/routes/UserRoutes.ts
+++ server/routes/UserRoutes.ts
@@ -1,11 +1,12 @@
 // UserRoutes.ts
-import { getUser, saveFloorPlan } from "../controllers/UserController";
+import { getUser, saveFloorPlan,getFloorPlan } from "../controllers/UserController";
 import express from 'express';
 const upload = require('../middleware/upload.ts');
 
 const router = express.Router();
 
-router.get("/getUser", getUser);
-router.post("/saveFloorPlan",upload.uploadFloorPlan.single('image'), saveFloorPlan);
+router.get("/get_user", getUser);
+router.post("/save_floor_plan",upload.uploadFloorPlan.single('image'), saveFloorPlan);
+router.get("/get_floor_plan/:plan_id", getFloorPlan);
 
 export { router };
